09f7554ea61c7476a01ada36a37f7f5615b349c0
Daniel Segal <dlemel8@gmail.com>
2021-05-21 18:38:49 +0300

    add tunnel type command line arg to generate TCP or DNS tunnel

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use std::net::IpAddr;
 
 use simple_logger::SimpleLogger;
-use structopt::StructOpt;
+use structopt::{clap::arg_enum, StructOpt};
 use tokio::net::{TcpListener, TcpStream};
 
 use crate::dns::DnsTunnel;
@@ -11,8 +11,19 @@ use crate::tunnel::{AsyncReadWrapper, AsyncWriteWrapper, TcpTunneler, Tunneler};
 mod dns;
 mod tunnel;
 
+arg_enum! {
+    #[derive(Debug, Copy, Clone)]
+    enum TunnelType {
+        TCP,
+        DNS,
+    }
+}
+
 #[derive(StructOpt, Debug)]
 struct Cli {
+    #[structopt(env, possible_values = & TunnelType::variants(), case_insensitive = true)]
+    tunnel_type: TunnelType,
+
     #[structopt(env)]
     remote_address: IpAddr,
 
@@ -45,10 +56,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     while let Ok((in_stream, in_address)) = listener.accept().await {
         log::debug!("got connection from {}", in_address);
+        let tunnel_type = args.tunnel_type;
         let remote_address = args.remote_address;
         let remote_port = args.remote_port;
         let task = async move {
-            if let Err(e) = handle_in_stream(remote_address, remote_port, in_stream).await {
+            if let Err(e) = handle_in_stream(tunnel_type, remote_address, remote_port, in_stream).await {
                 log::error!("failed to handle in stream: {}", e);
             }
         };
@@ -59,20 +71,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub(crate) async fn new_tunnel(address: IpAddr, port: u16) -> Result<Box<dyn Tunneler>, Box<dyn Error>> {
-    let tunnel = TcpTunneler::new(address, port).await?;
-    // let tunnel = DnsTunnel::new(address, port).await?;
-    return Ok(Box::new(tunnel));
+pub(crate) async fn new_tunneler(tunnel_type: TunnelType, address: IpAddr, port: u16) -> Result<Box<dyn Tunneler>, Box<dyn Error>> {
+    match tunnel_type {
+        TunnelType::TCP => Ok(Box::new(TcpTunneler::new(address, port).await?)),
+        TunnelType::DNS => Ok(Box::new(DnsTunnel::new(address, port).await?)),
+    }
 }
 
 async fn handle_in_stream(
+    tunnel_type: TunnelType,
     remote_address: IpAddr,
     remote_port: u16,
     in_stream: TcpStream,
 ) -> Result<(), Box<dyn Error>> {
     let (in_read, in_write) = in_stream.into_split();
 
-    let mut tunnel = new_tunnel(remote_address, remote_port).await?;
+    let mut tunnel = new_tunneler(tunnel_type, remote_address, remote_port).await?;
     let reader = Box::new(AsyncReadWrapper::new(in_read));
     let writer = Box::new(AsyncWriteWrapper::new(in_write));
     match tunnel.tunnel(reader, writer).await {
