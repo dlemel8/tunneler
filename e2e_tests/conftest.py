99cf9c67a1055f2a27272f5d4551833b705dd843
Daniel Segal <dlemel8@gmail.com>
2021-07-23 10:03:46 +0300

    e2e tests: only use python_on_whales and not docker-py + refactor fixtures

--- e2e_tests/conftest.py
+++ e2e_tests/conftest.py
@@ -1,16 +1,16 @@
 import asyncio
 from asyncio import StreamReader, StreamWriter
+from enum import Enum
 from os import getenv
 
 import pytest
-import python_on_whales
-from docker import DockerClient, from_env
-from docker.models.containers import Container
-from docker.models.images import Image
+from python_on_whales import docker, Image, Container
 
-BACKEND_PORT = 8080
-UNTUNNELER_PORT = 8899
-TUNNELER_PORT = 8888
+
+class TestPorts(Enum):
+    BACKEND_PORT = 8080
+    UNTUNNELER_PORT = 8899
+    TUNNELER_PORT = 8888
 
 
 async def echo_handler(reader: StreamReader, writer: StreamWriter) -> None:
@@ -31,7 +31,7 @@ async def echo_handler(reader: StreamReader, writer: StreamWriter) -> None:
 
 @pytest.fixture
 async def echo_backend_server():
-    server = await asyncio.start_server(echo_handler, '127.0.0.1', BACKEND_PORT)
+    server = await asyncio.start_server(echo_handler, '127.0.0.1', TestPorts.BACKEND_PORT.value)
     address = server.sockets[0].getsockname()
     print(f'serving backend on {address}')
 
@@ -42,77 +42,62 @@ async def echo_backend_server():
         await server.wait_closed()
 
 
-@pytest.fixture(scope='session')
-def docker_client() -> DockerClient:
-    yield from_env()
-
-
-@pytest.fixture(scope='session')
-def server_image(docker_client: DockerClient) -> Image:
+def build_tunneler_image(executable: str, image_name: str) -> Image:
     cache_from_registry = getenv('CACHE_FROM_REGISTRY')
     cache_from = f'type=registry,ref={cache_from_registry}' if cache_from_registry else None
-    image = python_on_whales.docker.build(context_path='.',
-                                          tags='test_server',
-                                          cache_from=cache_from,
-                                          cache_to='type=inline',
-                                          load=True,
-                                          build_args={'EXECUTABLE': 'server'})
+    return docker.build(
+        context_path='.',
+        tags=image_name,
+        cache_from=cache_from,
+        cache_to='type=inline',
+        load=True,
+        build_args={'EXECUTABLE': executable}
+    )
 
-    yield image
-    docker_client.images.remove(image.id, force=True)
 
-
-@pytest.fixture
-def server_container(docker_client: DockerClient, server_image: Image) -> Container:
-    container = docker_client.containers.run(
-        server_image.id,
-        name='test_server',
+def run_tunneler_container(image: Image, container_name: str, local_port: TestPorts, remote_port: TestPorts) -> Container:
+    return docker.run(
+        image,
+        name=container_name,
         detach=True,
-        environment={
+        envs={
             'TUNNEL_TYPE': 'Dns',
-            'LOCAL_PORT': UNTUNNELER_PORT,
-            'REMOTE_PORT': BACKEND_PORT,
+            'LOCAL_PORT': local_port.value,
+            'REMOTE_PORT': remote_port.value,
             'REMOTE_ADDRESS': '127.0.0.1',
             'LOG_LEVEL': 'debug',
         },
-        network_mode='host'
+        networks=['host'],
     )
+
+
+@pytest.fixture(scope='session')
+def server_image() -> Image:
+    image = build_tunneler_image('server', 'test_server')
+    yield image
+    docker.image.remove(image.id, force=True)
+
+
+@pytest.fixture
+def server_container(server_image: Image) -> Container:
+    container = run_tunneler_container(server_image, 'test_server', TestPorts.UNTUNNELER_PORT, TestPorts.BACKEND_PORT)
     yield container
     print(f'server {container.logs()=}')
-    container.stop(timeout=0)
+    container.kill()
     container.remove(force=True)
 
 
 @pytest.fixture(scope='session')
-def client_image(docker_client: DockerClient) -> Image:
-    cache_from_registry = getenv('CACHE_FROM_REGISTRY')
-    cache_from = f'type=registry,ref={cache_from_registry}' if cache_from_registry else None
-    image = python_on_whales.docker.build(context_path='.',
-                                          tags='test_client',
-                                          cache_from=cache_from,
-                                          cache_to='type=inline',
-                                          load=True,
-                                          build_args={'EXECUTABLE': 'client'})
+def client_image() -> Image:
+    image = build_tunneler_image('client', 'test_client')
     yield image
-    docker_client.images.remove(image.id, force=True)
+    docker.image.remove(image.id, force=True)
 
 
 @pytest.fixture
-def client_container(docker_client: DockerClient, client_image: Image) -> Container:
-    container = docker_client.containers.run(
-        client_image.id,
-        name='test_client',
-        detach=True,
-        environment={
-            'TUNNEL_TYPE': 'Dns',
-            'LOCAL_PORT': TUNNELER_PORT,
-            'REMOTE_PORT': UNTUNNELER_PORT,
-            'REMOTE_ADDRESS': '127.0.0.1',
-            'LOG_LEVEL': 'debug',
-        },
-        network_mode='host'
-    )
+def client_container(client_image: Image) -> Container:
+    container = run_tunneler_container(client_image, 'test_client', TestPorts.TUNNELER_PORT, TestPorts.UNTUNNELER_PORT)
     yield container
-    print(f'client {container.logs()=}')
-    container.stop(timeout=0)
+    print(f'server {container.logs()=}')
+    container.kill()
     container.remove(force=True)
--- e2e_tests/test_dns_tunnel.py
+++ e2e_tests/test_dns_tunnel.py
@@ -2,13 +2,13 @@ import asyncio
 
 import pytest
 
-from e2e_tests.conftest import TUNNELER_PORT
+from e2e_tests.conftest import TestPorts
 
 
 @pytest.mark.asyncio
-async def test_dns_tunnel_single_client_single_echo(echo_backend_server, server_container, client_container) -> None:
+async def test_single_client_single_echo(echo_backend_server, server_container, client_container) -> None:
     message_to_send = 'bla'
-    reader, writer = await asyncio.open_connection('127.0.0.1', TUNNELER_PORT)
+    reader, writer = await asyncio.open_connection('127.0.0.1', TestPorts.TUNNELER_PORT.value)
 
     writer.write(message_to_send.encode())
     await writer.drain()
@@ -21,9 +21,9 @@ async def test_dns_tunnel_single_client_single_echo(echo_backend_server, server_
 
 
 @pytest.mark.asyncio
-async def test_dns_tunnel_single_client_multiple_echo(echo_backend_server, server_container, client_container) -> None:
+async def test_single_client_multiple_echo(echo_backend_server, server_container, client_container) -> None:
     messages_to_send = ['bla', 'bli', 'blu']
-    reader, writer = await asyncio.open_connection('127.0.0.1', TUNNELER_PORT)
+    reader, writer = await asyncio.open_connection('127.0.0.1', TestPorts.TUNNELER_PORT.value)
 
     received_messages = []
     for message in messages_to_send:
