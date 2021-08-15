27e7518ddb355a5244d77ee42130589d504a035c
Daniel Segal <dlemel8@gmail.com>
2021-08-15 07:52:09 +0300

    use ansible to harden ssh on examples server

--- examples/speed_test/ansible.tf
+++ examples/speed_test/ansible.tf
@@ -1,4 +1,4 @@
-resource "local_file" "AnsibleInventory" {
+resource "local_file" "ansible_inventory" {
   content = templatefile("ansible_inventory.tmpl", {
     public-ip = linode_instance.tunneler_example.ip_address
   })
